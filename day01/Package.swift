d1784db8bc0a04a6f838a6403aa726459fa0e9b0
Will Fairclough <will@willdoes.dev>
2024-12-02 12:11:26 -0500

    Day 01 solution

--- day01/Package.swift
+++ day01/Package.swift
@@ -5,10 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "day1",
+    platforms: [
+        .macOS(.v14),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/davedufresne/SwiftParsec.git", from: "4.0.1")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "day1"),
+            name: "day1",
+            dependencies: ["SwiftParsec"]
+        ),
     ]
 )
