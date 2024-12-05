414ef1fd1876280dad880dc829519975fb3fa331
Will Fairclough <github@willdoes.dev>
2024-12-05 16:21:23 -0500

    Switch to cli tool with ParsableCommand for part1 and part2

--- day05/Sources/Day05.swift
+++ day05/Sources/Day05.swift
@@ -16,14 +16,25 @@ import ArgumentParser
 
 @main
 struct Day05: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "A utility for solving the Day05 puzzle of Advent of Code 2024",
+        subcommands: [Day05Part1.self, Day05Part2.self]
+    )
 
-    @Argument(help: "The input file for day 5.")
+}
+
+struct Day05Part1: ParsableCommand, FileInput {
+    static let configuration = CommandConfiguration(
+        commandName: "part1",
+        abstract: "Solve the 1st part of the Day05 puzzle"
+    )
+
+    @Option(name: [.short, .long], help: "The input file to use for the puzzle")
     var filename: String
 
     mutating func run() throws {
-        let input = try! String(contentsOfFile: self.filename, encoding: .utf8)
+        let input = getInput()
         var (updates, rules) = input.parseAsUpdateAndRules()
-        // print(rules)
         let splitIndex = updates.partition { $0.isValid(with: rules) }
         let invalidUpdates = updates[..<splitIndex].map { $0 }
         let validUpdates = updates[splitIndex...].map { $0 }
@@ -34,3 +45,30 @@ struct Day05: ParsableCommand {
         print("Sum of middle pages of valid updates: \(midPageSum)")
     }
 }
+
+
+struct Day05Part2: ParsableCommand, FileInput {
+    static let configuration = CommandConfiguration(
+        commandName: "part2",
+        abstract: "Solve the 2nd part of the Day05 puzzle"
+    )
+
+    @Option(name: [.short, .long], help: "The input file to use for the puzzle")
+    var filename: String
+
+    mutating func run() throws {
+        let input = getInput()
+        print(input)
+    }
+}
+
+protocol FileInput {
+    var filename: String { get set }
+}
+
+extension FileInput {
+    func getInput() -> String {
+        return try! String(contentsOfFile: self.filename, encoding: .utf8)
+    }
+}
+
